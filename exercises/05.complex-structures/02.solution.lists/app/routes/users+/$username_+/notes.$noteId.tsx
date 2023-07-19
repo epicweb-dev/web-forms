41fd976943ec225f5b6bd944f4f7bb960f227673
Kent C. Dodds <me+github@kentcdodds.com>
2023-07-19 10:11:41 -0600

    move images above the note contents

--- exercises/05.complex-structures/02.solution.lists/app/routes/users+/$username_+/notes.$noteId.tsx
+++ exercises/05.complex-structures/02.solution.lists/app/routes/users+/$username_+/notes.$noteId.tsx
@@ -51,9 +51,6 @@ export default function NoteRoute() {
 		<div className="absolute inset-0 flex flex-col px-10">
 			<h2 className="mb-2 pt-12 text-h2 lg:mb-6">{data.note.title}</h2>
 			<div className="overflow-y-auto pb-24">
-				<p className="whitespace-break-spaces text-sm md:text-lg">
-					{data.note.content}
-				</p>
 				<ul className="flex flex-wrap gap-5 py-5">
 					{data.note.images.map(image => (
 						<li key={image.id}>
@@ -67,6 +64,9 @@ export default function NoteRoute() {
 						</li>
 					))}
 				</ul>
+				<p className="whitespace-break-spaces text-sm md:text-lg">
+					{data.note.content}
+				</p>
 			</div>
 			<div className={floatingToolbarClassName}>
 				<Form method="post">
