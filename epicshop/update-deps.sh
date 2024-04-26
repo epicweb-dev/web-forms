8170780a058f197b7a0c14ea77089e1f45e9d6eb
Kent C. Dodds <me+github@kentcdodds.com>
2024-04-26 09:17:18 -0600

    add remix-flat-routes to deps

--- epicshop/update-deps.sh
+++ epicshop/update-deps.sh
@@ -1,6 +1,8 @@
-npx npm-check-updates --dep prod,dev --upgrade --workspaces --root
-rm -rf node_modules package-lock.json ./exercises/**/node_modules
+npx npm-check-updates --dep prod,dev --upgrade --workspaces --root --reject eslint
+cd epicshop && npx npm-check-updates --dep prod,dev --upgrade --workspaces --root --reject eslint
+cd ..
+rm -rf node_modules package-lock.json ./epicshop/package-lock.json ./epicshop/node_modules ./exercises/**/node_modules
 npm install
-node setup.js
+npm run setup
 npm run typecheck
 npm run lint --fix
